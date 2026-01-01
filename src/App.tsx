72f6e1e6983cb73e16d33bdbfac08fe1aaae8853
javierOrtega95 <fjortega@wizzie.io>
2026-01-01 23:41:18 +0100

    fix: adjust layout for consistent minimum height across sections

--- src/App.tsx
+++ src/App.tsx
@@ -24,12 +24,12 @@ export default function App() {
   }
 
   return (
-    <div className='flex flex-col h-screen bg-gray-50'>
+    <div className='flex flex-col min-h-screen lg:h-screen bg-gray-50'>
       <Header />
 
-      <main className='flex flex-col flex-1 min-h-0 max-w-[1600px] mx-auto w-full px-4 sm:px-6 lg:px-8 py-6'>
-        <div className='grid grid-cols-1 lg:grid-cols-12 gap-6 flex-1 min-h-0'>
-          <section className='lg:col-span-7 flex flex-col gap-4 min-h-0'>
+      <main className='flex flex-col flex-1 max-w-[1600px] mx-auto w-full px-4 sm:px-6 lg:px-8 py-6 lg:min-h-0'>
+        <div className='grid grid-cols-1 lg:grid-cols-12 gap-6 lg:flex-1 lg:min-h-0'>
+          <section className='lg:col-span-7 flex flex-col gap-4 min-h-[50vh] lg:min-h-0'>
             <EditorToolbar
               selectedExample={selectedExample}
               onExampleChange={handleExampleChange}
@@ -40,7 +40,7 @@ export default function App() {
             <EditorPanel code={code} fileName={selectedExample.filename} onChange={setCode} />
           </section>
 
-          <section className='lg:col-span-5 flex flex-col min-h-0'>
+          <section className='lg:col-span-5 flex flex-col min-h-[50vh] lg:min-h-0'>
             <OutputPanel result={executionResult} status={executionStatus} />
           </section>
         </div>
